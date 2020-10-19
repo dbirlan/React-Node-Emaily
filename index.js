65ca9973bc2cd02914ffc1c1ef730e3205bfb5a5
Daniel <daniel.birlan@gmail.com>
2020-10-20 00:18:20 +0200

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'fuck you bros' });
+  res.send({ bye: 'fuck you bross' });
+  res.send({ bye: 'fuck you dude' });
 });
 
 const PORT = process.env.PORT || 5000;
