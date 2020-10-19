538a8796c7f61b4ab2921a38575f30e70fdc0709
Daniel <daniel.birlan@gmail.com>
2020-10-20 00:13:35 +0200

    well that's a modification

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'fuck you bro' });
+  res.send({ bye: 'fuck you bros' });
 });
 
 const PORT = process.env.PORT || 5000;
