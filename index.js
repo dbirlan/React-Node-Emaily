172aa9f186ebddcb835992b9eb8893421bfd6e46
Daniel <daniel.birlan@gmail.com>
2020-10-20 00:09:54 +0200

    commitments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'broooooos' });
+  res.send({ bye: 'fuck you bro' });
 });
 
 const PORT = process.env.PORT || 5000;
