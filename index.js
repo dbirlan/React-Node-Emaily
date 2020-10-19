87f99bc682ca9abfdfb0a0dd22b794634412835f
Daniel <daniel.birlan@gmail.com>
2020-10-19 23:53:27 +0200

    modification

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
+  res.send({ bye: 'friend' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// git add .
+// git commit -m ""
+// git push heroku master
