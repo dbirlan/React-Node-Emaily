0cb0c7318cfd829a9141718d68a02af97c338ac3
Daniel <daniel.birlan@gmail.com>
2020-10-28 00:31:16 +0100

    small update

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'bye-bye buddy' });
+  res.send({ bye: 'bye-bye friend' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// git push -u origin main            (for github push)
