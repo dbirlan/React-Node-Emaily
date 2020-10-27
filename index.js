44e81eae512133c27922507988e1e8cf210ddd38
Daniel <daniel.birlan@gmail.com>
2020-10-28 00:32:25 +0100

    friend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'bye-bye friend' });
+  res.send({ bye: 'bye-bye my friend' });
 });
 
 const PORT = process.env.PORT || 5000;
