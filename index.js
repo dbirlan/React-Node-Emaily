6cac0044e591071ec411e2f5188308e07ea5cb11
Daniel <daniel.birlan@gmail.com>
2020-11-01 18:52:53 +0100

    google authentification finalized

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,19 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+//Express needs to add in the cookie function
+app.use(
+  cookieSession({
+    // How long until the cookie expires (in ms, so 30 days x 24 ...)
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // Security key
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
