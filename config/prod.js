f30fb44f0f015a0c288543d0f938c6b00c1c2d27
Daniel <daniel.birlan@gmail.com>
2020-11-01 22:21:18 +0100

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// prod.js - production keys here
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+mongodb+srv://dbirlan01:erixwhS84lcnnz0L@emaily-prod.waq4a.mongodb.net/<dbname>?retryWrites=true&w=majority
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,9 @@ module.exports = (app) => {
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
