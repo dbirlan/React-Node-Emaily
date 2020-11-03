f66c9e08b3c4a33cbbd6ec7c26aef1fb9a90a414
Daniel <daniel.birlan@gmail.com>
2020-11-04 00:42:25 +0100

    wiring frontend

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
-export default function f(state = {}, action) {
+import {FETCH_USER} from '../actions/types';
+
+export default function f(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
