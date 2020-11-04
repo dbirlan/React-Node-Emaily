7b4a1738300e17d6dbbad6416c50420a629d4142
Daniel <daniel.birlan@gmail.com>
2020-11-04 21:29:34 +0100

    Landing update

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Spring } from 'react-spring/renderprops';
 
 const Landing = () => {
   return (
-    <div style={{textAlign: 'center'}}>
-      <h1>Emaily !</h1>
-      Collect feedback from your users
+    <div style={{ textAlign: 'center' }}>
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}
+        config={{ duration: 1000 }}
+      >
+        {(props) => <h1 style={props}>Emaily !</h1>}
+      </Spring>
+      <p>Collect feedback from your users</p>
     </div>
+    
   );
 };
 
